4c5041966ed8e91dbefb316dc9cc3d95c2e7c541
Ed Cres <crespoed07@gmail.com>
2021-06-09 18:48:38 -0400

    Created a new project, added viewpager2 graddle dependency, and changed main activity to take a viewPager2.

---
+++ test-viewpager2/app/src/main/java/com/example/testmyviewpager2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.testmyviewpager2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Viewpager2, Fragments, Tabs, Material Design
+// https://www.androidhive.info/2020/01/viewpager2-pager-transformations-intro-slider-pager-animations-pager-transformations/
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+// steps:
+/*
+- add dependency:   implementation "androidx.viewpager2:viewpager2:1.0.0"
+ */
