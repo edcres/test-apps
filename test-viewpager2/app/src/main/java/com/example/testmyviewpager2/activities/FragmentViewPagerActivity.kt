a8f323b1ec7bd00b4ccacfcf3cb5e5a1b819c8a9
Ed Cres <crespoed07@gmail.com>
2021-06-10 18:28:03 -0400

    Tab layout with: viewPager2, material design, and fragments is done.

--- test-viewpager2/app/src/main/java/com/example/testmyviewpager2/activities/FragmentViewPagerActivity.kt
+++ test-viewpager2/app/src/main/java/com/example/testmyviewpager2/activities/FragmentViewPagerActivity.kt
@@ -1,12 +1,57 @@
 package com.example.testmyviewpager2.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.testmyviewpager2.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.testmyviewpager2.databinding.ActivityFragmentViewPagerBinding
+import com.example.testmyviewpager2.fragments.EventsFragment
+import com.example.testmyviewpager2.fragments.MoviesFragment
+import com.example.testmyviewpager2.fragments.TicketsFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FragmentViewPagerActivity : AppCompatActivity() {
+    var binding: ActivityFragmentViewPagerBinding? = null
+
+    // tab titles
+    private val titles = arrayOf("Movies", "Events", "Tickets")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment_view_pager)
+        binding = ActivityFragmentViewPagerBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        init()
+    }
+
+    private fun init() {
+        // removing toolbar elevation
+        supportActionBar!!.elevation = 0f
+        binding?.viewPager?.adapter = ViewPagerFragmentAdapter(this)
+
+        // attaching tab mediator
+        binding?.let { TabLayoutMediator(binding!!.tabLayout, it.viewPager) {
+                tab: TabLayout.Tab, position: Int ->
+                tab.text = titles[position]
+            }.attach()
+        }
+
+    }
+
+    // adapter for the viewpager
+    private inner class ViewPagerFragmentAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
+        override fun createFragment(position: Int): Fragment {
+            when (position) {
+                0 -> return MoviesFragment()
+                1 -> return EventsFragment()
+                2 -> return TicketsFragment()
+            }
+            return MoviesFragment()
+        }
+
+        override fun getItemCount(): Int {
+            return titles.size
+        }
     }
 }
--- test-viewpager2/app/src/main/java/com/example/testmyviewpager2/activities/MainActivity.kt
+++ test-viewpager2/app/src/main/java/com/example/testmyviewpager2/activities/MainActivity.kt
@@ -1,21 +1,28 @@
-package com.example.testmyviewpager2
+package com.example.testmyviewpager2.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testmyviewpager2.R
 
 // Viewpager2, Fragments, Tabs, Material Design
 // https://www.androidhive.info/2020/01/viewpager2-pager-transformations-intro-slider-pager-animations-pager-transformations/
+// setPageTransformer() to change the animation of transitions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this@MainActivity, FragmentViewPagerActivity::class.java))
     }
 }
-
-
 
 // steps:
 /*
 - add dependency:   implementation "androidx.viewpager2:viewpager2:1.0.0"
+- add fragments
+- add fragments.xml
+- add viewpager activity
+- add viewpager activity.xml
  */
