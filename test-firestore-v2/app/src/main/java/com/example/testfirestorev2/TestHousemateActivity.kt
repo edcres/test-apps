a1de3adcf712038e16ab02c5fd0560f8fc338a04
Ed Cres <crespoed07@gmail.com>
2021-08-04 20:37:48 -0400

    UI widgets binded.

--- test-firestore-v2/app/src/main/java/com/example/testfirestorev2/TestHousemateActivity.kt
+++ test-firestore-v2/app/src/main/java/com/example/testfirestorev2/TestHousemateActivity.kt
@@ -4,14 +4,16 @@ import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-// create the database through the client once, but get rid of that code after the database is created
-// need a hashmap for groupIDs, clientIDs and shoppingItemContents
+// Create the database through the client once, but get rid of that code after the database is created.
+// Need a hashmap for groupIDs, clientIDs and shoppingItemContents.
 
 // todo:
 // -set up database
@@ -22,11 +24,58 @@ class TestHousemateActivity : AppCompatActivity() {
 
     private val db = Firebase.firestore
     private lateinit var editTextTest: EditText
-    private lateinit var buttonTest: Button
-    private lateinit var buttonGetData: Button
 
     private var clientGroupIDCollection = "abcd1234"
     private var clientIDCollection = "${clientGroupIDCollection}abcd1234"
+
+    private lateinit var i1shoppingItIsDone: CheckBox
+    private lateinit var i1shoppingItemQty: TextView
+    private lateinit var i1shoppingItemName: TextView
+    private lateinit var i1shoppingWhenNeededDoneText: TextView
+    private lateinit var i1shoppingWhereText: TextView
+    private lateinit var i1shoppingCostText: TextView
+    private lateinit var i1shoppingWhoIsGettingItText: TextInputEditText
+    private lateinit var i1shoppingPriorityText: TextView
+    private lateinit var i1shoppingAddedByText: TextView
+    private lateinit var i2shoppingItIsDone: CheckBox
+    private lateinit var i2shoppingItemQty: TextView
+    private lateinit var i2shoppingItemName: TextView
+    private lateinit var i2shoppingWhenNeededDoneText: TextView
+    private lateinit var i2shoppingWhereText: TextView
+    private lateinit var i2shoppingCostText: TextView
+    private lateinit var i2shoppingWhoIsGettingItText: TextInputEditText
+    private lateinit var i2shoppingPriorityText: TextView
+    private lateinit var i2shoppingAddedByText: TextView
+    private lateinit var i3shoppingItIsDone: CheckBox
+    private lateinit var i3shoppingItemQty: TextView
+    private lateinit var i3shoppingItemName: TextView
+    private lateinit var i3shoppingWhenNeededDoneText: TextView
+    private lateinit var i3shoppingWhereText: TextView
+    private lateinit var i3shoppingCostText: TextView
+    private lateinit var i3shoppingWhoIsGettingItText: TextInputEditText
+    private lateinit var i3shoppingPriorityText: TextView
+    private lateinit var i3shoppingAddedByText: TextView
+    private lateinit var i1choresItIsDone: CheckBox
+    private lateinit var i1choresItemName: TextView
+    private lateinit var i1choresWhenNeededDoneText: TextView
+    private lateinit var i1choresDifficulty: TextView
+    private lateinit var i1choresWhoIsDoingItText: TextInputEditText
+    private lateinit var i1choresPriorityText: TextView
+    private lateinit var i1choresAddedByText: TextView
+    private lateinit var i2choresItIsDone: CheckBox
+    private lateinit var i2choresItemName: TextView
+    private lateinit var i2choresWhenNeededDoneText: TextView
+    private lateinit var i2choresDifficulty: TextView
+    private lateinit var i2choresWhoIsDoingItText: TextInputEditText
+    private lateinit var i2choresPriorityText: TextView
+    private lateinit var i2choresAddedByText: TextView
+    private lateinit var i3choresItIsDone: CheckBox
+    private lateinit var i3choresItemName: TextView
+    private lateinit var i3choresWhenNeededDoneText: TextView
+    private lateinit var i3choresDifficulty: TextView
+    private lateinit var i3choresWhoIsDoingItText: TextInputEditText
+    private lateinit var i3choresPriorityText: TextView
+    private lateinit var i3choresAddedByText: TextView
 
     companion object {
         private const val TAG = "TestHousemateActyTAG"
@@ -34,7 +83,9 @@ class TestHousemateActivity : AppCompatActivity() {
         private const val GROUPS_DOC = "groupIDs"
         private const val CLIENTS_DOC = "clientIDs"
         private const val SHOPPING_LIST = "shoppingList"
+        private const val SHOPPING_ITEMS_COLLECTION = "Shopping items"
         private const val CHORES_LIST = "choresList"
+        private const val CHORE_ITEMS_COLLECTION = "Chore items"
 
         private const val ID_FIELD = "id"
         private const val NAME_FIELD = "name"
@@ -56,16 +107,10 @@ class TestHousemateActivity : AppCompatActivity() {
 
         bindWidgetIDs()
 
-        buttonOnClick()
-        getButtonOnClick()
+        buttonClickListeners()
     }
 
-
-
-
-
-
-    // todo: delete this
+    // todo: delete these
     private fun testTheDb() {
         //collection
         val docOne = hashMapOf(
@@ -115,15 +160,15 @@ class TestHousemateActivity : AppCompatActivity() {
         // get documents, I think it comes back in the form of a hashMap
         // get the data from the document
         Log.d(TAG, "testTheDbGet: called")
-        val theEditText: EditText = findViewById(R.id.edit_text_test)
+//        val theEditText: EditText = findViewById(R.id.edit_text_test)
         var retrievedDocDataMap: HashMap<String, Any>? = null
         db.collection("data").document("one")
             .get().addOnSuccessListener { document ->
                 if (document != null) {
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
                     retrievedDocDataMap = document.data as HashMap<String, Any>
-                    val thePie = retrievedDocDataMap!!["numberExample"]  //todo: 'thePopulation' might always be null
-                    theEditText.setText(thePie.toString())
+                    val thePie = retrievedDocDataMap!!["numberExample"]
+//                    theEditText.setText(thePie.toString())
                 } else {
                     Log.d(TAG, "No such document")
                 }
@@ -142,28 +187,12 @@ class TestHousemateActivity : AppCompatActivity() {
         }
     }
 
-
-
-
-
-
-
     // CLICK LISTENERS //
-    private fun buttonOnClick() {
-        buttonTest.setOnClickListener {
+    private fun buttonClickListeners() {
 //            val testString = editTextTest.text//.toString()
 //            Log.d(TAG, "$testString")
-            testTheDb()     //
-        }
+//            testTheDb()     //
     }
-    private fun getButtonOnClick() {
-        buttonGetData.setOnClickListener{
-            testTheDbGet()  //
-        }
-    }
-
-
-
 
     // HELPER FUNCTIONS //
     private fun addShoppingItem(
@@ -191,6 +220,7 @@ class TestHousemateActivity : AppCompatActivity() {
         db.collection(GENERAL_COLLECTION).document(GROUPS_DOC)
             .collection(clientGroupIDCollection).document(CLIENTS_DOC)
             .collection(clientIDCollection).document(SHOPPING_LIST)
+            .collection(SHOPPING_ITEMS_COLLECTION).document(itemName)
             .set(shoppingItemData)
             .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
             .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
@@ -217,6 +247,7 @@ class TestHousemateActivity : AppCompatActivity() {
         db.collection(GENERAL_COLLECTION).document(GROUPS_DOC)
             .collection(clientGroupIDCollection).document(CLIENTS_DOC)
             .collection(clientIDCollection).document(CHORES_LIST)
+            .collection(CHORE_ITEMS_COLLECTION).document(itemName)
             .set(choresItemData)
             .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
             .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
@@ -224,8 +255,59 @@ class TestHousemateActivity : AppCompatActivity() {
 
     // SETUP FUNCTIONS //
     private fun bindWidgetIDs() {
-        editTextTest = findViewById(R.id.edit_text_test)
-        buttonTest = findViewById(R.id.button_test)
-        buttonGetData = findViewById(R.id.button_test_2)
+
+        i1shoppingItIsDone = findViewById(R.id.i1shopping_it_is_done)
+        i1shoppingItemQty = findViewById(R.id.i1shopping_item_qty)
+        i1shoppingItemName = findViewById(R.id.i1shopping_item_name)
+        i1shoppingWhenNeededDoneText = findViewById(R.id.i1shopping_when_needed_done_text)
+        i1shoppingWhereText = findViewById(R.id.i1shopping_where_text)
+        i1shoppingCostText = findViewById(R.id.i1shopping_cost_text)
+        i1shoppingWhoIsGettingItText = findViewById(R.id.i1shopping_who_is_getting_it_text)
+        i1shoppingPriorityText = findViewById(R.id.i1shopping_priority_text)
+        i1shoppingAddedByText = findViewById(R.id.i1shopping_added_by_text)
+
+        i2shoppingItIsDone = findViewById(R.id.i2shopping_it_is_done)
+        i2shoppingItemQty = findViewById(R.id.i2shopping_item_qty)
+        i2shoppingItemName = findViewById(R.id.i2shopping_item_name)
+        i2shoppingWhenNeededDoneText = findViewById(R.id.i2shopping_when_needed_done_text)
+        i2shoppingWhereText = findViewById(R.id.i2shopping_where_text)
+        i2shoppingCostText = findViewById(R.id.i2shopping_cost_text)
+        i2shoppingWhoIsGettingItText = findViewById(R.id.i2shopping_who_is_getting_it_text)
+        i2shoppingPriorityText = findViewById(R.id.i2shopping_priority_text)
+        i2shoppingAddedByText = findViewById(R.id.i2shopping_added_by_text)
+
+        i3shoppingItIsDone = findViewById(R.id.i3shopping_it_is_done)
+        i3shoppingItemQty = findViewById(R.id.i3shopping_item_qty)
+        i3shoppingItemName = findViewById(R.id.i3shopping_item_name)
+        i3shoppingWhenNeededDoneText = findViewById(R.id.i3shopping_when_needed_done_text)
+        i3shoppingWhereText = findViewById(R.id.i3shopping_where_text)
+        i3shoppingCostText = findViewById(R.id.i3shopping_cost_text)
+        i3shoppingWhoIsGettingItText = findViewById(R.id.i3shopping_who_is_getting_it_text)
+        i3shoppingPriorityText = findViewById(R.id.i3shopping_priority_text)
+        i3shoppingAddedByText = findViewById(R.id.i3shopping_added_by_text)
+
+        i1choresItIsDone = findViewById(R.id.i1chores_it_is_done)
+        i1choresItemName = findViewById(R.id.i1chores_item_name)
+        i1choresWhenNeededDoneText = findViewById(R.id.i1chores_when_needed_done_text)
+        i1choresDifficulty = findViewById(R.id.i1chores_difficulty)
+        i1choresWhoIsDoingItText = findViewById(R.id.i1chores_who_is_doing_it_text)
+        i1choresPriorityText = findViewById(R.id.i1chores_priority_text)
+        i1choresAddedByText = findViewById(R.id.i1chores_added_by_text)
+
+        i2choresItIsDone = findViewById(R.id.i2chores_it_is_done)
+        i2choresItemName = findViewById(R.id.i2chores_item_name)
+        i2choresWhenNeededDoneText = findViewById(R.id.i2chores_when_needed_done_text)
+        i2choresDifficulty = findViewById(R.id.i2chores_difficulty)
+        i2choresWhoIsDoingItText = findViewById(R.id.i2chores_who_is_doing_it_text)
+        i2choresPriorityText = findViewById(R.id.i2chores_priority_text)
+        i2choresAddedByText = findViewById(R.id.i2chores_added_by_text)
+
+        i3choresItIsDone = findViewById(R.id.i3chores_it_is_done)
+        i3choresItemName = findViewById(R.id.i3chores_item_name)
+        i3choresWhenNeededDoneText = findViewById(R.id.i3chores_when_needed_done_text)
+        i3choresDifficulty = findViewById(R.id.i3chores_difficulty)
+        i3choresWhoIsDoingItText = findViewById(R.id.i3chores_who_is_doing_it_text)
+        i3choresPriorityText = findViewById(R.id.i3chores_priority_text)
+        i3choresAddedByText = findViewById(R.id.i3chores_added_by_text)
     }
 }
