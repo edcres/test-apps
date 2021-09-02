6c7f2d938bc1e0a30e6b1bba947df4c076c76cb8
Ed Cres <crespoed07@gmail.com>
2021-09-02 10:26:15 -0400

    Setting up for populating UI and fetching data

--- test-firebase-rtdb/app/src/main/java/com/example/testfirebaseroomcache/cutomerorders/OrdersActivity.kt
+++ test-firebase-rtdb/app/src/main/java/com/example/testfirebaseroomcache/cutomerorders/OrdersActivity.kt
@@ -1,12 +1,18 @@
 package com.example.testfirebaseroomcache.cutomerorders
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.testfirebaseroomcache.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -19,6 +25,7 @@ import com.google.firebase.ktx.Firebase
 
 class OrdersActivity : AppCompatActivity() {
 
+    private lateinit var sharedPref: SharedPreferences
     private val db = Firebase.database.reference
 
     private lateinit var orderNumTxt: TextView
@@ -31,10 +38,14 @@ class OrdersActivity : AppCompatActivity() {
     private lateinit var removeLocationBtn: Button
 
     companion object {
-        // orders db -> location(h6872) -> orders ()
-        private const val ORDERS_DB_NODE = "Orders Node"
+        private const val TAG = "OrdersActyTag"
+        private const val LOCATION_ID_SP_TAG = "Location id"
+        // orders db -> Location -> h6872 -> orders ()
+        private const val ORDERS_DB_NODE = "Orders"
         private const val LOCATION_NODE = "Location"
-        private const val ORDERS_NODE = "Orders"
+        private var locationID: String? = null
+//        private const val ORDERS_NODE = "Orders"
+        private var orderNumber = ""
         private const val ORDER_NUMBER_FIELD = "Number"
         private const val DETAILS_FIELD = "Details"
         private const val NOTE_FIELD = "Note"
@@ -46,11 +57,59 @@ class OrdersActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orders)
 
+        sharedPref = this.getSharedPreferences("OrdersActivitySP", Context.MODE_PRIVATE)
         bindUIWidgets()
+        //get location id
+        getDataFromSPAndSetUpUI()
         clickListeners()
     }
 
     // HELPER FUNCTIONS //
+    private fun getDataFromSPAndSetUpUI() {
+        // try to get from SP, if null, pop a dialog box
+        locationID = sharedPref.getString(LOCATION_ID_SP_TAG, null)
+
+        if (locationID == null) {
+            // pop a dialog box and have user type the location id
+            makeDialogBoxAndSetLocationID()
+        } else {
+            // todo:
+            //  get data from db
+            //  populate the UI
+            //  display store ID
+        }
+    }
+    @SuppressLint("ApplySharedPref")
+    private fun sendDataToSP(theLocationId: String, sPTag: String) {
+        // save location id tp SP
+        val spEditor: SharedPreferences.Editor = sharedPref.edit()
+        spEditor.putString(LOCATION_ID_SP_TAG, theLocationId).commit()
+    }
+    @SuppressLint("ApplySharedPref")
+    private fun clearSP() {
+        // removes the store number from SP
+        sharedPref.edit().clear().commit()
+    }
+    private fun makeDialogBoxAndSetLocationID() {
+        val stringInputDialog = MaterialAlertDialogBuilder(this)
+        val customAlertDialogView = LayoutInflater.from(this)
+            .inflate(R.layout.string_dialog_box, null, false)
+        val inputStringDialog:EditText= customAlertDialogView.findViewById(R.id.string_input_dialog)
+        inputStringDialog.hint = "H7428"
+        stringInputDialog.setView(customAlertDialogView)
+            .setTitle("Store number")
+            .setPositiveButton("Accept") { dialog,  _ ->
+                locationID = inputStringDialog.text.toString()
+                Log.d(TAG, "makeDialogBoxAndSetLocationID: accept clicked $locationID")
+                sendDataToSP(locationID!!, LOCATION_ID_SP_TAG)
+                // todo:
+                //  get data from db
+                //  populate the UI
+                //  display store ID
+                dialog.dismiss()
+            }
+            .show()
+    }
     private fun breakDateFormat(dateString: String): List<Int> {
         var monthDayYear: List<Int>
         dateString.split("/").let { (month, day, year) ->
@@ -83,7 +142,6 @@ class OrdersActivity : AppCompatActivity() {
             } else {
                 dateAddedTxt.toString()
             }
-
             val customerOrder = CustomerOrder(
                 orderNumTxt.text.toString(),
                 orderDetailsEdtTxt.text.toString(),
@@ -92,14 +150,16 @@ class OrdersActivity : AppCompatActivity() {
             )
 
             // todo: send it to db
-
+            // todo: get/generate order number
+            db.child(ORDERS_DB_NODE).child(LOCATION_NODE).child(locationID).child(orderNumber)
+                .setValue(customerOrder)
         }
         removeOrderBtn.setOnClickListener {
             // todo: remove that order from the db
             orderNumTxt.text.toString()
         }
         removeLocationBtn.setOnClickListener {
-            // clear shared preferences (the store number <ie. H6872>)
+            clearSP()
         }
     }
     // SETUP FUNCTIONS //
