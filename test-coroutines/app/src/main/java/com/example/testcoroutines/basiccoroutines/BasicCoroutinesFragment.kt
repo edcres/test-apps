d17fe4f4516a1eb0b26564a5292fe91152e3ef20
Ed Cres <crespoed07@gmail.com>
2021-09-25 15:50:21 -0400

    Did some coroutine example.

--- test-coroutines/app/src/main/java/com/example/testcoroutines/basiccoroutines/BasicCoroutinesFragment.kt
+++ test-coroutines/app/src/main/java/com/example/testcoroutines/basiccoroutines/BasicCoroutinesFragment.kt
@@ -10,7 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.example.testcoroutines.R
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,13 +22,15 @@ import kotlinx.coroutines.withContext
 // suspend functions should only be called from coroutine scopes or other suspend function
 // scoping is a way to organize coroutines into groupings
 
+// make a db request, get a result, use that result to make another db request
+
 class BasicCoroutinesFragment : Fragment() {
 
     private val TAG = "BasicCoroutinesFgt Tag"
     private val RESULT_1 = "Result #1"
     private val RESULT_2 = "Result #2"
-    private lateinit var parallelTxt: TextView
-    private lateinit var parallelBtn: Button
+    private lateinit var basicsTxt: TextView
+    private lateinit var basicsBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,25 +40,38 @@ class BasicCoroutinesFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_basic_coroutines, container, false)
         bindUIWidgets(view)
 
-        parallelBtn.setOnClickListener {
-            CoroutineScope(IO).launch {
+        basicsBtn.setOnClickListener {
+            // " CoroutineScope(IO).launch {}" creates a coroutine
+            CoroutineScope(Default).launch {
+                logThread("onCreateView")
                 fakeAPIRequest()
             }
         }
         return view
     }
 
+    // API //
     private suspend fun fakeAPIRequest() {
         val result1 = getResult1FromAPI()
         Log.d(TAG, "debug: $result1")
         setTextOnMainThread(result1)
+
+        // when needing the first result (result1) to use in the second suspend fun
+        // it waits until the previous result (result1) is ready when the suspend functions
+        // are called within the same coroutines, and with the same suspend fun (fakeAPIRequest)
+        val result2 = getResult2FromAPI(result1)
+        setTextOnMainThread(result2)
     }
+    // API //
 
     // have to set the text from the Main thread, not the IO thread
     private suspend fun setTextOnMainThread(input: String) {
+        // calling withContext in a coroutine switches the thread to the mew one
+        // the thread only switches to main while within the curly brackets {}
         withContext(Main) {
-            val textToDisplay = "\n${parallelTxt.text} $input"
-            parallelTxt.text = textToDisplay
+            logThread("setTextOnMainThread")
+            val textToDisplay = "${basicsTxt.text} $input\n"
+            basicsTxt.text = textToDisplay
         }
     }
 
@@ -68,13 +83,22 @@ class BasicCoroutinesFragment : Fragment() {
         return RESULT_1
     }
 
-    private fun logThread(methodName: String) {
-        // show what method is being called and what coroutine and thread the method is being executed on
-        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    private suspend fun getResult2FromAPI(result1: String): String {
+        logThread("getResult2FromAPI")
+        delay(1000)
+        return RESULT_2
     }
 
-    private fun bindUIWidgets(view: View) {
-        parallelTxt = view.findViewById(R.id.parallel_txt)
-        parallelBtn = view.findViewById(R.id.parallel_btn)
+    private fun logThread(methodName: String) {
+        // show what method is being called and what coroutine and thread the method is being executed on
+        Log.d("TAG", "debug: ${methodName}: ${Thread.currentThread().name}")
+//        println("debug: ${methodName}: ${Thread.currentThread().name}")
     }
+
+    // Bind Widgets //
+    private fun bindUIWidgets(view: View) {
+        basicsTxt = view.findViewById(R.id.basics_txt)
+        basicsBtn = view.findViewById(R.id.basics_btn)
+    }
+    // Bind Widgets //
 }
--- test-coroutines/app/src/main/java/com/example/testcoroutines/parallelasyncawait/ParallelAsyncAwait.kt
+++ test-coroutines/app/src/main/java/com/example/testcoroutines/parallelasyncawait/ParallelAsyncAwait.kt
@@ -1,21 +1,91 @@
 package com.example.testcoroutines.parallelasyncawait
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.testcoroutines.R
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 // https://www.youtube.com/watch?v=HPpiPzwQ_cU
 
 class ParallelAsyncAwait : Fragment() {
+
+    private val TAG = "Parallel TAG"
+    private lateinit var parallelBtn: Button
+    private lateinit var parallelTxt: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parallel_async_await, container, false)
+        val view = inflater.inflate(R.layout.fragment_parallel_async_await, container, false)
+        bindUIWidgets(view)
+
+        parallelBtn.setOnClickListener {
+            parallelTxt.text = "Clicked"
+            fakeAPIRequest()
+        }
+
+        return view
+    }
+
+    private fun fakeAPIRequest() {
+
+        CoroutineScope(IO).launch {
+            val executionTime = measureTimeMillis {
+                val result1: Deferred<String> = async {
+                    Log.d(TAG, "fakeAPIRequest: job1 thread: ${Thread.currentThread().name}")
+                    getResult1FromAPI()
+                }
+                val result2: Deferred<String> = async {
+                    Log.d(TAG, "fakeAPIRequest: job2 thread: ${Thread.currentThread().name}")
+                    getResult2FromAPI()
+                }
+
+                // calling '.await() gets the result from the function'
+                //      'setTextOnMainThread' will not execute until the result is ready
+                Log.d(TAG, "\"fakeAPIRequest Got ${result1}\"")     // Shows 'Active'
+                setTextOnMainThread("Got ${result1.await()}")
+                Log.d(TAG, "\"fakeAPIRequest Got ${result1}\"")     // Shows 'Completed'
+                setTextOnMainThread("Got ${result2.await()}")
+            }
+            Log.d(TAG, "debug: total time elapsed: $executionTime")
+        }
+    }
+
+    private suspend fun setTextOnMainThread(input: String) {
+        // the thread only switches to main while within the curly brackets {}
+        withContext(Main) {
+            Log.d(TAG, "switch to main: ${Thread.currentThread()}")
+            // the UI always needs to be updated in the Main thread, or the app will crash
+            val textToDisplay = "${parallelTxt.text} $input\n"
+            parallelTxt.text = textToDisplay
+        }
+        Log.d(TAG, "switch back: ${Thread.currentThread()}")
+    }
+
+    private suspend fun getResult1FromAPI(): String {
+        delay(2000)
+        return "Result #1"
+    }
+
+    private suspend fun getResult2FromAPI(): String {
+        delay(1000)
+        return "Result #2"
+    }
+
+    // BIND WIDGETS //
+    private fun bindUIWidgets(view: View) {
+        parallelBtn = view.findViewById(R.id.parallel_btn)
+        parallelTxt = view.findViewById(R.id.parallel_txt)
     }
 }
