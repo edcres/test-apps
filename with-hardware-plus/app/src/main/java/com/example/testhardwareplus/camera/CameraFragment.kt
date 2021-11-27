e204629e28e57066da892b016aa3d5fbb2de8d65
Ed Cres <crespoed07@gmail.com>
2021-11-26 20:39:06 -0500

    Image Capture feature added.

--- with-hardware-plus/app/src/main/java/com/example/testhardwareplus/camera/CameraFragment.kt
+++ with-hardware-plus/app/src/main/java/com/example/testhardwareplus/camera/CameraFragment.kt
@@ -3,24 +3,34 @@ package com.example.testhardwareplus.camera
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.testhardwareplus.R
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 typealias LumaListener = (luma: Double) -> Unit
 
+// This code uses the CameraX library
 
 // https://www.youtube.com/watch?v=xZZQ5q5pOp0      I didn't use this
 // https://developer.android.com/codelabs/camerax-getting-started#0
@@ -36,6 +46,8 @@ class CameraFragment : Fragment() {
     private var imageCapture: ImageCapture? = null
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+    
+    private val imageURIs = mutableListOf<Uri>() // I made this to have a list of image locations
 
     companion object {
         private const val TAG = "CameraXBasic"
@@ -44,6 +56,7 @@ class CameraFragment : Fragment() {
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 
+    // OVERRIDE FUNCTIONS //
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -58,7 +71,8 @@ class CameraFragment : Fragment() {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this.requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                this.requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
         }
 
         cameraCaptureBtn.setOnClickListener { takePhoto() }
@@ -70,14 +84,119 @@ class CameraFragment : Fragment() {
         return view
     }
 
-    private fun takePhoto() {}
+    // should be replaced with 'registerForActivityResult(ActivityResultContract, ActivityResultCallback)'
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                // If permissions are not granted, present a toast to notify the user that the permissions were not granted
+                Toast.makeText(
+                    requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-    private fun startCamera() {}
+                // this is to destroy the fragment, the codeLab uses an activity
+                parentFragmentManager.beginTransaction().remove(this).commit()
+                // 'requireActivity().finish()'
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+    // OVERRIDE FUNCTIONS //
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            requireContext() /*baseContext*/, it) == PackageManager.PERMISSION_GRANTED
+            requireContext() /*baseContext*/, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
+
+    // FEATURES //
+    // preview the camera
+    // implement a viewfinder
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding, then bind new use cases to camera
+                cameraProvider.unbindAll()
+                cameraProvider
+                    .bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+
+
+        // for image capture feature
+        imageCapture = ImageCapture.Builder()
+            .build()
+
+    }
+
+    // Implement ImageCapture
+    // define a configuration object that is used to instantiate the actual use case object
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireContext()), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    imageURIs.add(savedUri) // I made this to have a list of image locations
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+
+    }
+    // FEATURES //
+
+    // SAVED IMAGE //
+    
+    // SAVED IMAGE //
 
     private fun getOutputDirectory(): File {
         val mediaDir = requireActivity().externalMediaDirs.firstOrNull()?.let {
@@ -85,11 +204,6 @@ class CameraFragment : Fragment() {
         }
         return if (mediaDir != null && mediaDir.exists())
             mediaDir else requireActivity().filesDir
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
     }
 
 }
