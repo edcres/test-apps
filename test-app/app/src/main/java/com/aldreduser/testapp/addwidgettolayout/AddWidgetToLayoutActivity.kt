4381481a5ed985deafc17a9a9b9cb7712bc801fe
Ed Cres <crespoed07@gmail.com>
2022-02-18 16:41:13 -0500

    Can add and remove layyouts inside a linearLayout.

--- test-app/app/src/main/java/com/aldreduser/testapp/addwidgettolayout/AddWidgetToLayoutActivity.kt
+++ test-app/app/src/main/java/com/aldreduser/testapp/addwidgettolayout/AddWidgetToLayoutActivity.kt
@@ -4,14 +4,18 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.LinearLayout
 import com.aldreduser.testapp.R
 import kotlinx.android.synthetic.main.layout_widget_item_to_add.view.*
 
 class AddWidgetToLayoutActivity : AppCompatActivity() {
 
-    private lateinit var addedWidgetContainers: LinearLayout
-    private val txtList = listOf("One", "Two", "Three", "Four")
+    private lateinit var addLayoutBtn: Button
+    private lateinit var removeLayoutBtn: Button
+    private lateinit var addedWidgetContainer: LinearLayout
+    private val txtList = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)
+    private var iterator = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,26 +23,35 @@ class AddWidgetToLayoutActivity : AppCompatActivity() {
 
         bindUIWidgets()
 
-        for(i in 1..4) {
-            addWidgetToLayout(i)
+        addLayoutBtn.setOnClickListener {
+            addWidgetToLayout()
+        }
+        removeLayoutBtn.setOnClickListener {
+            removeWidget()
         }
     }
 
-    private fun addWidgetToLayout(iterator: Int) {
+    private fun addWidgetToLayout() {
         val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//        val linearLayoutItem: ConstraintLayout = findViewById(R.id.linear_item_container)
-
         val linearLayoutItem = inflater.inflate(R.layout.layout_widget_item_to_add, null)
-//        var itemNumber: TextView = findViewById(R.id.item_number)
         val itemNumber = linearLayoutItem.item_number
         val itemTxt = linearLayoutItem.item_txt
         itemNumber.text = "$iterator"
-        itemTxt.text = txtList[iterator-1]
+        iterator ++
+        val textForView = "S${txtList[iterator-1]}S"
+        itemTxt.text = textForView
 
-        addedWidgetContainers.addView(linearLayoutItem)
+        addedWidgetContainer.addView(linearLayoutItem)
+    }
+
+    private fun removeWidget() {
+        addedWidgetContainer.removeViewAt(1)
+//        addedWidgetContainer.removeAllViews()
     }
 
     private fun bindUIWidgets() {
-        addedWidgetContainers = findViewById(R.id.added_widget_containers)
+        addLayoutBtn = findViewById(R.id.add_layout_btn)
+        removeLayoutBtn = findViewById(R.id.remove_layout_btn)
+        addedWidgetContainer = findViewById(R.id.added_widget_container)
     }
 }
