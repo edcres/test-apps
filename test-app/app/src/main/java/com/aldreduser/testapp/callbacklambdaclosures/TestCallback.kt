a0d0e948c577a343077d8dee8ce829467f54cc4b
Ed Cres <crespoed07@gmail.com>
2022-05-22 03:32:37 -0400

    Took some notes on lambdas and singletons

---
+++ test-app/app/src/main/java/com/aldreduser/testapp/callbacklambdaclosures/TestCallback.kt
@@ -0,0 +1,70 @@
+package com.aldreduser.testapp.callbacklambdaclosures
+
+/** Callback
+ * A call back is a function that is passed into another function.
+ *      This other function is expected to callback/ execute the argument at some given time
+ *
+ */
+
+/** Closures
+ *
+ * If a callback interface only has one function, a callback can typically be converted into a closure.
+ *      (or more generally, a lambda)
+ */
+
+/** Lambda
+ *
+ * A function that can be stored as a variable
+ */
+
+// https://www.youtube.com/watch?v=icqv4isFopo
+/**
+ * the main purpose of callbacks and closures are asynchronous programming and event listening
+ */
+
+fun main() {
+//    rollDice(1..6, 4, {
+//        println(it)
+//    })
+    // Below code is another way of writing the above code.
+    // can move lambda out of parenthesis bc it is the last argument it takes, it just looks better
+    rollDice(1..6, 4) {
+        println(it)
+    }
+}
+
+// taking a function as an argument (lambda)
+fun rollDice(
+    range: IntRange,
+    time: Int,      // how many tomed to roll the dice
+    callback: (result: Int) -> Unit        // returning 'Unit' means the same as returning nothing
+) {
+    for(i in 0 until time) {
+        val result = range.random()
+        callback(result)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
