742e2206a2a6bdb1f74fd341e05b2597515e91a4
Ed Cres <crespoed07@gmail.com>
2022-06-30 18:03:25 -0400

    Set up app navigation in test charts app.

--- test-charts/app/src/main/java/com/example/testcharts/MainActivity.kt
+++ test-charts/app/src/main/java/com/example/testcharts/MainActivity.kt
@@ -3,6 +3,18 @@ package com.example.testcharts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/** Libraries
+ *
+ * Most popular by far:
+ * - https://github.com/PhilJay/MPAndroidChart
+ *
+ * Probably easy to use
+ * - https://github.com/blackfizz/EazeGraph
+ *
+ * Relatively popular and relatively easy to use
+ * - https://github.com/lecho/hellocharts-android
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
