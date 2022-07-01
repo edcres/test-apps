8804c2d959f8e8c8e07266678d26cfb2490ee44a
Ed Cres <crespoed07@gmail.com>
2022-07-01 13:04:25 -0400

    Set up linear chart.

---
+++ test-charts/app/src/main/java/com/example/testcharts/FromMedium.kt
@@ -0,0 +1,180 @@
+package com.example.testcharts
+
+import android.graphics.Color
+import android.graphics.Typeface
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
+import java.util.*
+
+// https://malcolmmaima.medium.com/kotlin-implementing-a-barchart-and-piechart-using-mpandroidchart-8c7643c4ba75
+
+class FromMedium {
+
+//    private val MAX_X_VALUE = 13
+//    private val GROUPS = 2
+//    private val GROUP_1_LABEL = "Orders"
+//    private val GROUP_2_LABEL = ""
+//    private val BAR_SPACE = 0.1f
+//    private val BAR_WIDTH = 0.8f
+//    private var chart: LineChart? = null
+//    private var pieChart: PieChart? = null      // rename this to lineChart
+//    protected var tfRegular: Typeface? = null
+//    protected var tfLight: Typeface? = null
+//
+//    private val statValues: ArrayList<Float> = ArrayList()
+//
+//    protected val statsTitles = arrayOf(
+//        "Orders", "Inventory"
+//    )
+//
+//    private val calendar: Calendar = Calendar.getInstance()
+//    private val year = calendar.get(Calendar.YEAR)
+//
+//
+//
+//    init {
+//        getStats()
+//    }
+//
+//
+//    fun getStats() {
+//        val values1: ArrayList<BarEntry> = ArrayList()
+//        statValues.clear()
+//
+//        for (i in 0 until MAX_X_VALUE) {
+//            values1.add(
+//                BarEntry(
+//                    i.toFloat(),
+//                    (Math.random() * 80).toFloat()
+//                )
+//            )
+//        }
+//        displayData(values1)
+//    }
+//
+//    fun displayData(orderData: ArrayList<BarEntry>) {
+//        val data: BarData = createChartData(orderData)
+//        prepareChartData(data)
+//        configureBarChart()
+//        preparePieData()
+//    }
+//
+//    // todo use this for line chart INSTEAD of bar chart
+//    private fun prepareChartData(data: LineData) {
+//        chart!!.data = data
+//        chart!!.barData.barWidth = BAR_WIDTH
+//
+//        val groupSpace = 1f - (BAR_SPACE + BAR_WIDTH) //\* GROUPS  todo: fix this
+//        chart!!.groupBars(0f, groupSpace, BAR_SPACE)
+//        chart!!.invalidate()
+//    }
+//
+//    private fun configureBarChart() {
+//        chart!!.setPinchZoom(false)
+//        chart!!.setDrawBarShadow(false)
+//        chart!!.setDrawGridBackground(false)
+//
+//        chart!!.description.isEnabled = false
+//        val xAxis = chart!!.xAxis
+//        xAxis.granularity = 1f
+//        xAxis.setCenterAxisLabels(true)
+//        xAxis.setDrawGridLines(false)
+//        val leftAxis = chart!!.axisLeft
+//        leftAxis.setDrawGridLines(true)
+//        leftAxis.spaceTop = 35f
+//        leftAxis.axisMinimum = 0f
+//        chart!!.axisRight.isEnabled = false
+//        chart!!.xAxis.axisMinimum = 1f
+//        chart!!.xAxis.axisMaximum = MAX_X_VALUE.toFloat()
+//    }
+//
+//    private fun preparePieData() {
+//        // pie chart
+//        pieChart!!.setUsePercentValues(true)
+//        pieChart!!.description.isEnabled = false
+//        pieChart!!.setExtraOffsets(5F, 10F, 5F, 5F)
+//
+//        pieChart!!.dragDecelerationFrictionCoef = 0.95f
+//
+//        pieChart!!.setCenterTextTypeface(tfLight)
+//        pieChart!!.centerText = "generateCenterSpannableText()"
+//
+//        pieChart!!.isDrawHoleEnabled = true
+//        pieChart!!.setHoleColor(Color.WHITE)
+//
+//        pieChart!!.setTransparentCircleColor(Color.WHITE)
+//        pieChart!!.setTransparentCircleAlpha(110)
+//
+//        pieChart!!.holeRadius = 58f
+//        pieChart!!.transparentCircleRadius = 61f
+//
+//        pieChart!!.setDrawCenterText(true)
+//
+//        pieChart!!.rotationAngle = 0.toFloat()
+//        // enable rotation of the chart by touch
+//        pieChart!!.isRotationEnabled = true
+//        pieChart!!.isHighlightPerTapEnabled = true
+//
+//        pieChart!!.animateY(1400, Easing.EaseInOutQuad)
+//        // pieChart.spin(2000, 0, 360);
+//
+//        pieChart!!.spin(2000, 0F, 360F, Easing.EaseInOutQuad)
+//        val l = pieChart!!.legend
+//        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+//        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+//        l.orientation = Legend.LegendOrientation.VERTICAL
+//        l.setDrawInside(false)
+//        l.xEntrySpace = 7f
+//        l.yEntrySpace = 0f
+//        l.yOffset = 0f
+//
+//        // entry label styling
+//        pieChart!!.setEntryLabelColor(Color.WHITE)
+//        pieChart!!.setEntryLabelTypeface(tfRegular)
+//        pieChart!!.setEntryLabelTextSize(12f)
+//    }
+//
+//    fun setPieChartData(orderData: ArrayList<PieEntry>, inventoryData: ArrayList<PieEntry>) {
+//        val entries: ArrayList<PieEntry> = ArrayList()
+//
+//        entries.add(PieEntry(orderData[0].value, "Orders"))
+//        entries.add(PieEntry(inventoryData[0].value, "Inventory"))
+//
+//        //you can test above by adding random dummy data to the pie chart or passing the data from the backend.
+//
+//        //entries.add(PieEntry(random.nextInt(100).toFloat(), "Orders"))
+//        //entries.add(PieEntry(random.nextInt(100).toFloat(), "Inventory"))
+//
+//        val dataSet = PieDataSet(entries, "")
+//        dataSet.setDrawIcons(false)
+//        dataSet.sliceSpace = 3f
+//        dataSet.iconsOffset = MPPointF(0F, 40F)
+//        dataSet.selectionShift = 5f
+//
+//        // add colors
+//        val colors: ArrayList<Int> = ArrayList()
+//
+//        colors.add(ColorTemplate.rgb( getString(R.color.red.toInt()) ))
+//        colors.add(ColorTemplate.rgb( getString(R.color.blue.toInt()) ))
+//
+//        colors.add(ColorTemplate.getHoloBlue())
+//        dataSet.colors = colors
+//        //dataSet.setSelectionShift(0f);
+//        val data = PieData(dataSet)
+//        data.setValueFormatter(PercentFormatter())
+//        data.setValueTextSize(11f)
+//        data.setValueTextColor(Color.WHITE)
+//        data.setValueTypeface(tfLight)
+//        pieChart!!.setData(data)
+//
+//        // undo all highlights
+//        pieChart!!.highlightValues(null)
+//        pieChart!!.invalidate()
+//    }
+}
---
+++ test-charts/app/src/main/java/com/example/testcharts/mpandroid/MPAndroidChartFragment.kt
@@ -0,0 +1,247 @@
+package com.example.testcharts.mpandroid
+
+import android.graphics.Color
+import android.graphics.DashPathEffect
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.testcharts.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.Legend.LegendForm
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IFillFormatter
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.Utils
+
+class MPAndroidChartFragment : Fragment(), OnChartValueSelectedListener {
+
+    private lateinit var lineChartBtn: Button
+    private lateinit var pieChartBtn: Button
+    private lateinit var lineContainer: LinearLayout
+    private lateinit var pieContainer: LinearLayout
+    private lateinit var lineChart: LineChart
+    private lateinit var pieChart: PieChart
+
+    private lateinit var xAxis: XAxis
+    private lateinit var yAxis: YAxis
+    private lateinit var llXAxis: LimitLine
+    private lateinit var ll1: LimitLine
+    private lateinit var ll2: LimitLine
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_m_p_android_chart, container, false)
+
+        lineChartBtn = view.findViewById(R.id.line_chart_btn)
+        pieChartBtn = view.findViewById(R.id.pie_chart_btn)
+        lineContainer = view.findViewById(R.id.line_container)
+        pieContainer = view.findViewById(R.id.pie_container)
+        lineChart = view.findViewById(R.id.line_chart_view)
+        pieChart = view.findViewById(R.id.pie_chart_view)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lineChartBtn.setOnClickListener {
+            pieContainer.visibility = View.GONE
+            lineContainer.visibility = View.VISIBLE
+        }
+        pieChartBtn.setOnClickListener {
+            lineContainer.visibility = View.GONE
+            pieContainer.visibility = View.VISIBLE
+        }
+
+        makeLineChart()
+        makePieChart()
+    }
+
+    // the 2 below are for OnChartValueSelectedListener
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        // todo: test this
+        Log.i("Entry selected", e.toString())
+        Log.i("LOW HIGH", "low: " + lineChart.lowestVisibleX + ", high: " + lineChart.highestVisibleX)
+        Log.i("MIN MAX", "xMin: " + lineChart.xChartMin + ", xMax: " + lineChart.xChartMax + ", yMin: " + lineChart.yChartMin + ", yMax: " + lineChart.yChartMax)
+    }
+    override fun onNothingSelected() {
+        // todo: test this
+        Log.i("Nothing selected", "Nothing selected.")
+    }
+
+    private fun makeLineChart() {
+        lineChart.setBackgroundColor(resources.getColor(R.color.white))
+//        lineChart.description.isEnabled = false
+        lineChart.setTouchEnabled(true)
+
+        lineChart.setOnChartValueSelectedListener(this)
+        lineChart.setDrawGridBackground(false)
+
+        // create marker to display box when values are selected
+        val mv = MyMarkerView(requireContext(), R.layout.custom_marker_view)
+        // Set the marker to the chart
+        mv.chartView = lineChart
+        lineChart.marker = mv
+
+        // enable scaling and dragging
+        lineChart.isDragEnabled = true
+        lineChart.setScaleEnabled(true)
+        // chart.setScaleXEnabled(true);
+        // chart.setScaleYEnabled(true);
+
+        // force pinch zoom along both axis
+        lineChart.setPinchZoom(true)
+
+        xAxis = lineChart.xAxis
+        xAxis.enableGridDashedLine(10f, 10f, 0f)
+
+        yAxis = lineChart.axisLeft
+        // disable dual axis (only use LEFT axis)
+        lineChart.axisRight.isEnabled = false
+        // horizontal grid lines
+        yAxis.enableGridDashedLine(10f, 10f, 0f)
+        // axis range
+        yAxis.axisMaximum = 200f
+        yAxis.axisMinimum = -50f
+
+        // Create Limit Lines
+        llXAxis = LimitLine(9f, "Index 10")
+        llXAxis.lineWidth = 4f
+        llXAxis.enableDashedLine(10f, 10f, 0f)
+        llXAxis.labelPosition = LimitLabelPosition.RIGHT_BOTTOM
+        llXAxis.textSize = 10f
+        llXAxis.typeface = tfRegular
+        ll1 = LimitLine(150f, "Upper Limit")
+        ll1.lineWidth = 4f
+        ll1.enableDashedLine(10f, 10f, 0f)
+        ll1.labelPosition = LimitLabelPosition.RIGHT_TOP
+        ll1.textSize = 10f
+        ll1.typeface = tfRegular
+        ll2 = LimitLine(-30f, "Lower Limit")
+        ll2.lineWidth = 4f
+        ll2.enableDashedLine(10f, 10f, 0f)
+        ll2.labelPosition = LimitLabelPosition.RIGHT_BOTTOM
+        ll2.textSize = 10f
+        ll2.typeface = tfRegular
+        // draw limit lines behind data instead of on top
+        yAxis.setDrawLimitLinesBehindData(true)
+        xAxis.setDrawLimitLinesBehindData(true)
+        // add limit lines
+        yAxis.addLimitLine(ll1)
+        yAxis.addLimitLine(ll2)
+        //xAxis.addLimitLine(llXAxis);
+
+        // add data
+        setLinearData(45, 180f)
+        // draw points over time
+        lineChart.animateX(1500)
+        // get the legend (only possible after setting data)
+        val l: Legend = lineChart.legend
+        // draw legend entries as lines
+        l.form = LegendForm.LINE
+    }
+
+    private fun makePieChart() {
+    }
+
+    // Generate dummy values for the data
+    fun setLinearData(count: Int, range: Float) {
+
+        var set1: LineDataSet?
+        val values = ArrayList<Entry>()
+
+        for (i in 0 until count) {
+            val value = (Math.random() * range).toFloat() - 30
+            values.add(Entry(i.toFloat(), value, resources.getDrawable(R.drawable.star)))
+//            values.add(Entry(i, value, ResourcesCompat.getDrawable(R.drawable.star)))
+        }
+
+        if (lineChart.data != null && lineChart.data.dataSetCount > 0) {
+            // If data has already been created.
+            set1 = lineChart.data.getDataSetByIndex(0) as LineDataSet
+            set1.values = values
+            set1.notifyDataSetChanged()
+            lineChart.data.notifyDataChanged()
+            lineChart.notifyDataSetChanged()
+        } else {
+            // create a dataset and give it a type
+            set1 = LineDataSet(values, "DataSet 1")
+            set1.setDrawIcons(false)
+            // draw dashed line
+            set1.enableDashedLine(10f, 5f, 0f)
+            // black lines and points
+            set1.color = Color.BLACK
+            set1.setCircleColor(Color.BLACK)
+            // line thickness and point size
+            set1.lineWidth = 1f
+            set1.circleRadius = 3f
+            // draw points as solid circles
+            set1.setDrawCircleHole(false)
+            // customize legend entry
+            set1.formLineWidth = 1f
+            set1.formLineDashEffect = DashPathEffect(floatArrayOf(10f, 5f), 0f)
+            set1.formSize = 15f
+            // text size of values
+            set1.valueTextSize = 9f
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f)
+            // set the filled area
+            set1.setDrawFilled(true)
+            set1.fillFormatter =
+                IFillFormatter { dataSet, dataProvider -> lineChart.axisLeft.axisMinimum }
+            // set color of filled area
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.fade_red)
+                set1.fillDrawable = drawable
+            } else {
+                set1.fillColor = Color.BLACK
+            }
+            val dataSets = ArrayList<ILineDataSet>()
+            dataSets.add(set1) // add the data sets
+            // create a data object with the data sets
+            val data = LineData(dataSets)
+            // set data
+            lineChart.data = data
+        }
+    }
+
+    fun setPieData() {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
