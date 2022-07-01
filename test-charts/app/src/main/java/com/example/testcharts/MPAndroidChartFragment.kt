13284924fa7d8265abc1969acb32fec6f69f2dc0
Ed Cres <crespoed07@gmail.com>
2022-06-30 23:45:53 -0400

    Set up containers to display the charts.

--- test-charts/app/src/main/java/com/example/testcharts/MPAndroidChartFragment.kt
+++ test-charts/app/src/main/java/com/example/testcharts/MPAndroidChartFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 
 class MPAndroidChartFragment : Fragment() {
 
-    private lateinit var lineGraphBtn: Button
+    private lateinit var lineChartBtn: Button
     private lateinit var pieChartBtn: Button
+    private lateinit var lineContainer: LinearLayout
+    private lateinit var pieContainer: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,14 +21,18 @@ class MPAndroidChartFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_m_p_android_chart, container, false)
 
-        lineGraphBtn = view.findViewById(R.id.line_graph_btn)
+        lineChartBtn = view.findViewById(R.id.line_chart_btn)
         pieChartBtn = view.findViewById(R.id.pie_chart_btn)
+        lineContainer = view.findViewById(R.id.line_container)
+        pieContainer = view.findViewById(R.id.pie_container)
 
-        lineGraphBtn.setOnClickListener {
-
+        lineChartBtn.setOnClickListener {
+            pieContainer.visibility = View.GONE
+            lineContainer.visibility = View.VISIBLE
         }
         pieChartBtn.setOnClickListener {
-
+            lineContainer.visibility = View.GONE
+            pieContainer.visibility = View.VISIBLE
         }
 
         return view
