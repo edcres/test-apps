e1f0c8f9a6dd973ffc5b5c8e4ceba86b6365023c
Ed Cres <crespoed07@gmail.com>
2022-12-05 08:45:17 -0500

    Set up the foundation for a recyclerview with a recyclerview.adapter

--- test-recyclerview/app/src/main/java/com/example/testrecyclerview/recyclerdotadapter/RecyDotAdaptAdapter.kt
+++ test-recyclerview/app/src/main/java/com/example/testrecyclerview/recyclerdotadapter/RecyDotAdaptAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.testrecyclerview.recyclerdotadapter
 
-class RecyDotAdaptAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testrecyclerview.R
+import com.example.testrecyclerview.onewidget.Package
+
+class RecyDotAdaptAdapter(
+    private val packages: List<Package>
+) : RecyclerView.Adapter<RecyDotAdaptAdapter.RecyDotAdaptViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyDotAdaptViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_1, parent, false)
+        return RecyDotAdaptViewHolder(itemView, packages)
+    }
+
+    override fun onBindViewHolder(holder: RecyDotAdaptViewHolder, position: Int) {
+        packages[position]
+    }
+
+    override fun getItemCount(): Int = packages.size
+
+    class RecyDotAdaptViewHolder constructor(itemView: View, packages: List<Package>) :
+        RecyclerView.ViewHolder(itemView) {
+            init {
+                val thisPackage = packages[adapterPosition]
+                val itemText1: TextView = itemView.findViewById(R.id.item_text_1)
+                val itemText2: TextView = itemView.findViewById(R.id.item_sub_text)
+                itemText1.text = thisPackage.id.toString()
+                itemText2.text = thisPackage.name
+            }
+    }
 }
