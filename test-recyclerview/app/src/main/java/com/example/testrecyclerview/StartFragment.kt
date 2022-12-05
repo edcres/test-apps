6ed0c06de11deac98346501df6e3c6074f32ceaa
Ed Cres <crespoed07@gmail.com>
2022-12-05 07:22:41 -0500

    Set up the navigation to the other test recycler fragments.

--- test-recyclerview/app/src/main/java/com/example/testrecyclerview/StartFragment.kt
+++ test-recyclerview/app/src/main/java/com/example/testrecyclerview/StartFragment.kt
@@ -14,6 +14,9 @@ private const val TAG = "StartFrag_TAG"
 class StartFragment : Fragment() {
 
     private lateinit var lAAdapterBtn: Button
+    private lateinit var oneWidgetBtn: Button
+    private lateinit var widgetsInterfaceBtn: Button
+    private lateinit var recyclerDotAdapterBtn: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +25,25 @@ class StartFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_start, container, false)
 
         lAAdapterBtn = view.findViewById(R.id.l_a_adapter_btn)
+        oneWidgetBtn = view.findViewById(R.id.one_widget_btn)
+        widgetsInterfaceBtn = view.findViewById(R.id.widgets_interface_btn)
+        recyclerDotAdapterBtn = view.findViewById(R.id.recycler_dot_adapter_btn)
 
         lAAdapterBtn.setOnClickListener {
             val navController = Navigation.findNavController(requireParentFragment().requireView())
             navController.navigate(R.id.action_startFragment_to_LAFragment)
+        }
+        oneWidgetBtn.setOnClickListener {
+            val navController = Navigation.findNavController(requireParentFragment().requireView())
+            navController.navigate(R.id.action_startFragment_to_oneWidgetInterfaceFragment)
+        }
+        widgetsInterfaceBtn.setOnClickListener {
+            val navController = Navigation.findNavController(requireParentFragment().requireView())
+            navController.navigate(R.id.action_startFragment_to_clickInterfaceFragment)
+        }
+        recyclerDotAdapterBtn.setOnClickListener {
+            val navController = Navigation.findNavController(requireParentFragment().requireView())
+            navController.navigate(R.id.action_startFragment_to_recyDotAdapterFragment)
         }
 
         return view
--- test-recyclerview/app/src/main/java/com/example/testrecyclerview/recyclerdotadapter/RecyDotAdapterFragment.kt
+++ test-recyclerview/app/src/main/java/com/example/testrecyclerview/recyclerdotadapter/RecyDotAdapterFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.testrecyclerview.R
 
+// test things like updating all or updating one item or updating deletions
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
