7c82f817b4403e7466243b544dbf579683ae04b3
Eduardo A. Crespo <crespoed07@gmail.com>
2023-04-03 05:12:23 -0400

    Edited the flutter todo test for bugs.

--- flutter/todo_list/actual_todo_list/flutter_todos/lib/main_development.dart
+++ flutter/todo_list/actual_todo_list/flutter_todos/lib/main_development.dart
@@ -1,6 +1,21 @@
-import 'package:flutter_todos/app/app.dart';
-import 'package:flutter_todos/bootstrap.dart';
+// import 'package:flutter_todos/app/app.dart';
+// import 'package:flutter_todos/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+// void main() {
+//   bootstrap(() => const App());
+// }
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_todos/bootstrap.dart';
+import 'package:local_storage_todos_api/local_storage_todos_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final todosApi = LocalStorageTodosApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
+
+  bootstrap(todosApi: todosApi);
 }
