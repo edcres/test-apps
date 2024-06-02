ed964b0e9b5542945791c251be799e76f147f6fc
Eduardo A. Crespo <crespoed07@gmail.com>
2024-06-02 18:51:16 -0400

    Made basic flutter todo list.

---
+++ flutter/basics/very_basic_todo_list/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _promptAddTodoItem() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('New task'),
+          content: TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              Navigator.pop(context);
+              _addTodoItem(val);
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText, int index) {
+    return ListTile(
+      title: Text(todoText),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple To-Do List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _promptAddTodoItem,
+        tooltip: 'Add task',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
