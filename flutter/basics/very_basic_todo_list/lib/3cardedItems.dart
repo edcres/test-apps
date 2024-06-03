6132d84b28d3ee9af1ea4a8c8752f39955929cbb
Eduardo A. Crespo <crespoed07@gmail.com>
2024-06-02 23:28:17 -0400

    List items are in cards.

---
+++ flutter/basics/very_basic_todo_list/lib/3cardedItems.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() => _todoItems.add(task));
+    }
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _promptAddTodoItem() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('New task'),
+          content: TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              Navigator.pop(context);
+              _addTodoItem(val);
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemCount: _todoItems.length,
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return Column(
+            children: [
+              _buildTodoItem(_todoItems[index], index),
+              Divider(height: 1.0),
+            ],
+          );
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText, int index) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      elevation: 2.0,
+      child: ListTile(
+        title: Text(todoText),
+        trailing: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () => _removeTodoItem(index),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple To-Do List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _promptAddTodoItem,
+        tooltip: 'Add task',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
