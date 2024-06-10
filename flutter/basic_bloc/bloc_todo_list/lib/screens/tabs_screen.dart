6208fd4c1879edec16e8e02d02d229d94c9e195e
Eduardo A. Crespo <crespoed07@gmail.com>
2024-06-09 22:44:43 -0400

    Removed commented out imports.

--- flutter/basic_bloc/bloc_todo_list/lib/screens/tabs_screen.dart
+++ flutter/basic_bloc/bloc_todo_list/lib/screens/tabs_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/todo_bloc.dart';
 import 'items_screen.dart';
-// import '../models/todo.dart';
 
 class TabsScreen extends StatelessWidget {
   @override
